74bf56382e8489738b941a5ea7751cded26cd96c
anuradhabandara8 <nipunav@nekfa.com>
2023-06-05 11:46:45 +0530

    Added AppApprove

---
+++ api-firecertificate/api-firecertificate/Controllers/FireAppApproveController.cs
@@ -0,0 +1,30 @@
+﻿using api_rate.Filters;
+using api_rate.Models;
+using api_rate.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api_rate.Controllers
+{
+    [JwtCustomAuth]
+    public class FireAppApproveController : ApiController
+    {
+        private IAppSubmit _appSubmit = null;
+        private IGetData _getData = null;
+        
+        public FireAppApproveController(IAppSubmit IAppSubmit, IGetData IGetData)
+        {
+            _appSubmit = IAppSubmit;
+            _getData = IGetData;
+        }
+
+        public ReturnMsgInfo Post() 
+        {
+            return null;
+        }
+    }
+}
--- api-firecertificate/api-firecertificate/Helpers/IAppSubmit.cs
+++ api-firecertificate/api-firecertificate/Helpers/IAppSubmit.cs
@@ -9,7 +9,9 @@ namespace api_rate.Helpers
 {
     public interface IAppSubmit
     {
+        // Validate values before submitting application
         bool ValidateApplication(FireCertificateApplication objAppDetails, ref ReturnMsgInfo returnMsg);
+        // Save application to database tbl_firecertificate_application table 
         bool SaveApplication(FireCertificateApplication objAppDetails, ref ReturnMsgInfo returnMsg);
     }
 }
